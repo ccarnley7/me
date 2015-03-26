ff2752095767b5a426d916fc9407fe090ffd4e08
Christian Carnley <ccarnley7@gmail.com>
2015-03-26 14:49:58 -0600

    new way to do stuff

--- hello.js
+++ hello.js
@@ -1,14 +1,10 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();
-    }).listen(8080);
-});
+var server = http.createServer(function(request, response){
+	response.writeHeader(200, {"Content-Type" : "text/html"})
+	fs.createReadStream('/index.html').pipe(response);
+})
+
+server.listen(8080);
